7e3eec07c86af6e7fa86b638a0a00f9be713ecf9
dcjones1 <dcejjones@gmail.com>
2019-01-20 20:50:58 -0500

    Done.

--- Rakefile
+++ Rakefile
@@ -4,3 +4,7 @@
 require File.expand_path('../config/application', __FILE__)
 
 ArelLab::Application.load_tasks
+
+task :console => :environment do
+  Pry.start
+end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    max_length = Classification.joins(boat_classifications: :boat).maximum("boats.length")
+    Classification.joins(boat_classifications: :boat).where("length = '#{max_length}'")
+  end
 end
